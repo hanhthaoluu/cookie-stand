cb38632a80b02f381402f0fd1b918b28ec0058ee
hanhthaoluu <hanhthaoluu@gmail.com>
2017-06-08 21:09:07 -0700

    successfully calculated and store amounts of cookies purchased for each hour at the firstAndPike location

--- app.js
+++ app.js
@@ -2,22 +2,49 @@ var firstAndPike = {
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   averageCookiesPerCustomer: 6.3,
-  randomNumberOfCustomersPerHour: function(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  simulatedAmountsOfCookiesPurchasedForEachHour: 
-    (averageCookiesPerCustomer * randomNumberOfCustomersPerHour)
+  randomNumberOfCustomersPerHour: function(){
+    return Math.floor(Math.random() * (firstAndPike.maxHourlyCustomers - firstAndPike.minHourlyCustomers + 1)) + firstAndPike.minHourlyCustomers;
   }
 }
+
+firstAndPike.randomNumberOfCustomersPerHour();
+
+  var firstAndPikeSimulatedAmountsOfCookiesPurchasedPerDay = [];
+
+  var storeNumberHoursOpen = 14;
+
+    for(var i = 0; i < 14; i++){
+      var firstAndPikeSimulatedAmountsOfCookiesPurchasedForEachHour =
+      Math.floor(firstAndPike.averageCookiesPerCustomer * firstAndPike.randomNumberOfCustomersPerHour());
+      firstAndPikeSimulatedAmountsOfCookiesPurchasedPerDay.push(firstAndPikeSimulatedAmountsOfCookiesPurchasedForEachHour);
+    }
+
+console.log(firstAndPikeSimulatedAmountsOfCookiesPurchasedPerDay);
+
 
 var seaTacAirport = {
   minHourlyCustomers: 3,
   maxHourlyCustomers: 24,
   averageCookiesPerCustomer: 1.2,
-  randomNumberOfCustomersPerHour: function(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  randomNumberOfCustomersPerHour: function(){
+    return Math.floor(Math.random() * (seaTacAirport.maxHourlyCustomers - seaTacAirport.minHourlyCustomers + 1)) + seaTacAirport.minHourlyCustomers;
   }
 }
+
+seaTacAirport.randomNumberOfCustomersPerHour();
+
+  var seaTacAirportSimulatedAmountsOfCookiesPurchasedPerDay = [];
+
+  var storeNumberHoursOpen = 14;
+
+    for(var i = 0; i < 14; i++){
+      var seaTacAirportSimulatedAmountsOfCookiesPurchasedForEachHour =
+      Math.floor(seaTacAirport.averageCookiesPerCustomer * seaTacAirport.randomNumberOfCustomersPerHour());
+      seaTacAirportSimulatedAmountsOfCookiesPurchasedPerDay.push(seaTacAirportSimulatedAmountsOfCookiesPurchasedForEachHour);
+    }
+
+console.log(simulatedAmountsOfCookiesPurchasedPerDay);
+
 
 var seattleCenter = {
   minHourlyCustomers: 11,
